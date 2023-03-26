03f60454c32d57da4c4e4f86938652facd460e13
bubaleh1337 <ekaterina.pyshkova@gmail.com>
2023-03-26 19:16:32 +0500

    deleted draft file, updated final solution for task1

---
+++ task/Program.cs
@@ -0,0 +1,48 @@
+﻿string Prompt()
+{
+  Console.Write($"Enter string element: ");
+  string str = Console.ReadLine()!.ToString();
+  return str;
+}
+string[] GetArray()
+{
+  int size = new Random().Next(3, 6);
+  string[] array = new string[size];
+  for (int i = 0; i < size; i++)
+  {
+    array[i] = Prompt();
+  }
+  return array;
+}
+void PrintArray(string[] array)
+{
+  Console.Write($"[{String.Join(", ", array)}]");
+}
+
+void FindElement(string[] array)
+{
+  var result = new string[array.Length];
+  var count = 0;
+  foreach (var el in array)
+  {
+    if (el.Length <= 3)
+    {
+      result[count] = el;
+      count++;
+    }
+  }
+  if (result[0] == null)
+  {
+    Console.Write(" -> []");
+  }
+  else
+  {
+    Console.Write($" -> [{string.Join(", ", result, 0, count)}]");
+  }
+}
+
+Console.Clear();
+string[] array = GetArray();
+Console.WriteLine();
+PrintArray(array);
+FindElement(array);
